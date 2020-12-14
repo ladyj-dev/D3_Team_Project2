2fbc558a7c70399f19576ea3524d09f247777ad8
livillaranda <livillaranda@gmail.com>
2020-12-14 14:14:38 -0500

    Updating geomap files.

---
+++ google_maps.js
@@ -0,0 +1,52 @@
+// Variables & Constants
+// var winery = 
+
+// let map;
+// let service;
+// let infowindow;
+
+// Load JSON File
+d3.json("input_city.json").then(function (wineData) {
+  console.log(wineData);
+});
+
+
+
+function initMap() {
+  const link = `https://maps.googleapis.com/maps/api/js?key=${google_key}&libraries=places`;
+  const request = {
+    query: "Museum of Contemporary Art Australia",
+    fields: ["name", "geometry"],
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name);
+    infowindow.open(map);
+  });
+}
+// [END maps_place_search]
+
+
+
+// Create the tile layer that will be the background of our map
+// var figureoutamap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "light-v10", // find a map that works for visualization
+//   accessToken: mapbox_key
+// });
