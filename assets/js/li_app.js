62063317c5c0986c25cbeda882bf357606211b35
livillaranda <livillaranda@gmail.com>
2020-12-16 23:21:43 -0500

    Updating GeoMap files again.

--- assets/js/li_app.js
+++ assets/js/li_app.js
@@ -1,13 +1,14 @@
 // Map
-var worldMap = L.map("map", {
+var worldMap = L.map("myDiv", {
     center: [37.5452, -77.5407],
     zoom: 1
 });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}/?access_token=pk.eyJ1IjoibGl2aWxsYXJhbmRhIiwiYSI6ImNraTZtNHprdjB4ZXMycG53cGFoc3E4aHYifQ.AGwN1CpsOusH0sKpUHKOjg", {
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
-    id: "streets-v11"
+    id: "mapbox/streets-v11",
+    accessToken: "pk.eyJ1IjoibGl2aWxsYXJhbmRhIiwiYSI6ImNraTZtNHprdjB4ZXMycG53cGFoc3E4aHYifQ.AGwN1CpsOusH0sKpUHKOjg"
 }).addTo(worldMap);
 
 // Link to JSON in Github
@@ -26,6 +27,10 @@ d3.json(url).then(function (response) {
         // Add Each Location
         if (winery) {
             L.marker([winery.geometry.coordinates[1], winery.geometry.coordinates[0]])
+                
+                // Bind Winery Name in Popup
+                // .bindPopup("<h3>" + winery.properties.winery + "</h3><h3>Location: " + winery.geometry.coordinates[1] + "," + winery.geometry.coordinates[0] + "</h3>")  
+
                 // Add Markers to Map
                 .addTo(worldMap);
         }
@@ -49,5 +54,4 @@ d3.json(url).then(function (response) {
 // }).addTo(map)
 
 // // // POP UP FOR MAP
-// Bind Winery Name in Popup
-// .bindPopup("<h3>" + winery.properties.winery + "<h3><h3>Location: " + winery.geometry.coordinates[1] + "," + winery.geometry.coordinates[0] + "</h3>")                
+              
